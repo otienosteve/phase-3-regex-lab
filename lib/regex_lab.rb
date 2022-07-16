61ba6cece8310eb84a00de540826ddf152d30783
otienosteve <steveotieno701@gmail.com>
2022-07-16 21:28:51 +0300

    commit tree 2979e8aa1b0fd7f030aca00c86644cdc5437482c

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,11 +1,21 @@
 def starts_with_a_vowel?(word)
-
+    if word[0].match(/[aeiou]/)
+    true    
+   else
+       false
+   end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    text_to_search=text.split(" ")
+    found=text_to_search.filter do |word|
+       if word.scan(/\Ain\zing/)
+        return word
+       end
+    end
+    found
 end
-
+p words_starting_with_un_and_ending_with_ing("ungaring huko msee")
 def words_five_letters_long(text)
 
 end
